a099b8cd02d7f32643226218d1085c88f12afe59
Liviu Ionescu <ilg@livius.net>
2022-02-25 21:16:22 +0200

    add initial tests

---
+++ tests-xpack/platform-qemu-mps2-an386/cmake/globals.cmake
@@ -0,0 +1,67 @@
+#
+# This file is part of the µOS++ distribution.
+#   (https://github.com/micro-os-plus)
+# Copyright (c) 2022 Liviu Ionescu
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
+#
+# -----------------------------------------------------------------------------
+
+# This file defines the global compiler settings that apply to all targets.
+# Must be included with include() in the `tests` scope.
+
+message(VERBOSE "Including platform-qemu-mps2-an386 globals...")
+
+# -----------------------------------------------------------------------------
+
+set(xpack_device_compile_definition "DEVICE_QEMU_CORTEX_M4")
+
+# Global definitions.
+# add_compile_definitions()
+# include_directories()
+
+set(xpack_platform_common_options
+
+  -mcpu=cortex-m4
+  -mthumb
+  # -mfloat-abi=soft
+  -mfloat-abi=hard
+
+  -fno-move-loop-invariants
+
+  # https://cmake.org/cmake/help/v3.20/manual/cmake-generator-expressions.7.html?highlight=compile_language#genex:COMPILE_LANGUAGE
+  # $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>
+  $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>
+  $<$<COMPILE_LANGUAGE:CXX>:-fno-use-cxa-atexit>
+  $<$<COMPILE_LANGUAGE:CXX>:-fno-threadsafe-statics>
+
+  # -flto fails with undefined reference to `__assert_func'...
+  # $<$<CONFIG:Release>:-flto>
+  # $<$<CONFIG:MinSizeRel>:-flto>
+
+  # ... libs-c/src/stdlib/exit.c:132:46
+  # $<$<CXX_COMPILER_ID:GNU>:-Wno-missing-attributes>
+
+  # Embedded builds must be warning free.
+#!!  -Werror
+)
+
+add_compile_options(
+  ${xpack_platform_common_options}
+)
+
+add_compile_definitions(
+  # ...
+  _POSIX_C_SOURCE=200809L
+)
+
+# When `-flto` is used, the compile options must be passed to the linker too.
+add_link_options(
+  ${xpack_platform_common_options}
+)
+
+# -----------------------------------------------------------------------------
---
+++ tests-xpack/src/sample-test.cpp
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2022 Liviu Ionescu.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT/.
+ */
+
+// ----------------------------------------------------------------------------
+
+#if defined(MICRO_OS_PLUS_INCLUDE_CONFIG_H)
+#include <micro-os-plus/config.h>
+#endif // MICRO_OS_PLUS_INCLUDE_CONFIG_H
+
+#include <micro-os-plus/platform.h>
+
+// Include the implementation of the default main().
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+// ----------------------------------------------------------------------------
+
+// Simple examples of functions to be tested.
+static int
+compute_one (void)
+{
+  return 1;
+}
+
+static const char*
+compute_aaa (void)
+{
+  return "aaa";
+}
+
+static bool
+compute_condition (void)
+{
+  return true;
+}
+
+#if defined(__EXCEPTIONS)
+
+static void
+exercise_throw (bool mustThrow)
+{
+  if (mustThrow)
+    {
+      throw "kaboom";
+    }
+}
+
+#endif // defined(__EXCEPTIONS)
+
+// ----------------------------------------------------------------------------
+
+TEST_CASE( "Check various conditions" ) {
+  CHECK( compute_one() == 1 );
+  CHECK( strcmp(compute_aaa(), "aaa") == 0 );
+  CHECK( compute_condition() );
+}
+
+#if defined(__EXCEPTIONS)
+
+TEST_CASE( "Check exceptions" ) {
+  CHECK_THROWS( exercise_throw(true) );
+  CHECK_NOTHROW(  exercise_throw(false) );
+}
+
+#endif // defined(__EXCEPTIONS)
+
+// ----------------------------------------------------------------------------
